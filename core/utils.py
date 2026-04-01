dad0fa59ad098716949e27b279842a2c847e904f
Dade Murphy <Dade.murphy0742@gmail.com>
2026-04-01 14:37:25 -0400

    Update utils.py

--- core/utils.py
+++ core/utils.py
@@ -1,13 +1,28 @@
 import subprocess
 
-def run_command(command):
+def run_command(command, timeout=60):
     try:
-        result = subprocess.run(
+        process = subprocess.Popen(
             command,
             shell=True,
-            capture_output=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
             text=True
         )
-        return result.stdout.strip()
+
+        output_lines = []
+
+        for line in process.stdout:
+            print(line.strip())
+            output_lines.append(line.strip())
+
+        process.wait(timeout=timeout)
+
+        return "\n".join(output_lines)
+
+    except subprocess.TimeoutExpired:
+        process.kill()
+        return "[!] Command timed out"
+
     except Exception as e:
         return f"[ERROR] {str(e)}"
